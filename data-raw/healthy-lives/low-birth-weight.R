1baff9e486274e23a2dcb587a33e2a94ab4b6545
CatReid <CatrionaReid@redcross.org.uk>
2024-10-08 16:38:58 +0100

    Loaded low_birth-weight data to be cleaned
    hl_low_birth_weight data loaded and ready to be cleaned

--- data-raw/healthy-lives/low-birth-weight.R
+++ data-raw/healthy-lives/low-birth-weight.R
@@ -1,22 +1,27 @@
+# ---- Load packages ----
 library(readr)
 library(httr)
-library(dplyr)
+library(tidyverse)
+library(geographr)
 
-# Source:https://scotland.shinyapps.io/ScotPHO_profiles_tool/
+# ---- Get data ----
+# Source: https://scotland.shinyapps.io/ScotPHO_profiles_tool/
 # Indicator: Healthy birth weight
 
 # Interactively generate the data by:
-#   1. Navigating the the source (above)
+#   1. Navigating the Source (above)
 #   2. Clicking the 'Data' box
-#   3. Selecting the relevant indicator (above)
-#   4. Ticking 'All available geographies'
-#   5. Moving the time period slider until the latest data shows
-#   6. Right-clicking on 'Download data' and clicking 'Copy Link Location'
+#   3. 'Select what data you want'. Selecting the relevant indicator
+#      (Healthy birth weight)
+#   4. 'Select what areas you want'. Ticking 'All available geographies'
+#   5. 'Select the time period'. Moving the time period slider until the latest
+#      data shows (currently 2020-2023)
+#   6. Right-clicking on 'Download data' and clicking 'Copy Link'
 #   7. Pasting the link into the GET request below
 #   8. Running the code below
 
 GET(
-  "https://scotland.shinyapps.io/ScotPHO_profiles_tool/_w_1f998bbe/session/24180ffd708f74879b57a09212462e75/download/download_table_csv?w=1f998bbe",
+  "https://scotland.shinyapps.io/ScotPHO_profiles_tool/_w_197d7f90/session/4b5e695a2121c092892d20cd790b2fbc/download/download_table_csv?w=197d7f90",
   write_disk(tf <- tempfile(fileext = ".csv"))
 )
 
@@ -26,16 +31,23 @@ low_birth_weight_raw <-
 unlink(tf)
 rm(tf)
 
+#START HERE WED 9TH
+
+
+
+
+
+
 low_birth_weight <-
   low_birth_weight_raw %>%
   filter(area_type == "Council area") %>%
   select(
     lad_code = area_code,
     healthy_birth_weight_percent = measure
-  ) %>% 
+  ) %>%
   mutate(
     unhealthy_birth_weight_percent = (100 - healthy_birth_weight_percent)/100
-  ) %>% 
+  ) %>%
   select(-healthy_birth_weight_percent)
 
 write_rds(low_birth_weight, "data/vulnerability/health-inequalities/scotland/healthy-lives/low-birth-weight.rds")
