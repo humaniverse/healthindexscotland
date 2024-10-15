495c39e9d3fb33b136b60de8f761d3c183ca144c
CatReid <CatrionaReid@redcross.org.uk>
2024-10-15 17:11:36 +0100

    hl_vaccine_coverage script complete
    Completed hl_vaccine_coverage script. Still to write up data.R and metadata.

--- data-raw/healthy-lives/vaccination-coverage.R
+++ data-raw/healthy-lives/vaccination-coverage.R
@@ -1,174 +1,168 @@
-library(httr)
-library(readxl)
-library(dplyr)
+# ---- Load packages ----
 library(geographr)
-library(sf)
-library(stringr)
-library(tidyr)
-library(readr)
+library(tidyverse)
+library(rio)
+library(readxl)
+library(demographr)
 
-# Create Scotland LAD code to name lookup
-lookup <-
-  boundaries_lad %>%
-  as_tibble() %>%
-  select(lad_name, lad_code) %>%
-  filter(str_detect(lad_code, "^S")) %>%
-  arrange(lad_name)
-
+# ---- Get data ----
 # HPV
-# Source: https://beta.isdscotland.org/find-publications-and-data/population-health/child-health/hpv-immunisation-statistics-scotland/
-# Use Sheet 'Table 4b (S4- LA)', as it contains the cummaltive counts from S1-S4 (see footnotes)
-GET(
-  "https://beta.isdscotland.org/media/6895/2020-12-22-hpv-tables.xlsx",
-  write_disk(tf <- tempfile(fileext = ".xlsx"))
-)
+# Source: https://publichealthscotland.scot/publications/hpv-immunisation-statistics-scotland/hpv-immunisation-statistics-scotland-school-year-202223/
+ hpv_raw <- import("https://www.opendata.nhs.scot/dataset/272ce3f2-875b-4271-9a44-67486c1b59c4/resource/993b4c4d-996e-4fa2-b398-eebcd526e7ec/download/ca-hpv-immunisation-20231128.csv")
 
-hpv_raw <-
-  read_excel(tf, sheet = "Table 4b (S4- LA)", skip = 5)
-
-hpv <-
-  hpv_raw %>%
-  slice(1:32) %>%
-  select(lad_name = `...1`, hpv_vaccine_rate = `Uptake rate (%)...4`) %>%
-  mutate(hpv_vaccine_rate = as.double(hpv_vaccine_rate)) %>%
-  mutate(lad_name = if_else(lad_name == "Dumfries & Galloway", "Dumfries and Galloway", lad_name)) %>%
-  left_join(lookup, by = "lad_name") %>%
-  relocate(lad_code, .after = lad_name) %>%
-  select(-lad_name)
+hpv <- hpv_raw |>
+  filter(Sex == "Total") |>
+  group_by(CA) |>
+  mutate(
+    PercentCoverage = as.numeric(PercentCoverage),
+    total_av_percentage_coverage = sum(PercentCoverage, na.rm = TRUE) / 4
+  ) |>
+  ungroup() |>
+  rename(year = 1,
+         ltla19_code = 2) |>
+  select(`ltla19_code`, `total_av_percentage_coverage`, `year`) |>
+  slice(1:32)
 
 # Teenage booster immunisations
-# Source: https://beta.isdscotland.org/find-publications-and-data/population-health/child-health/teenage-booster-immunisation-statistics-scotland/
-GET(
-  "https://beta.isdscotland.org/media/6890/2020-12-22-teenage-booster-tables.xlsx",
-  write_disk(tf <- tempfile(fileext = ".xlsx"))
-)
+# Source: https://publichealthscotland.scot/publications/teenage-booster-immunisation-statistics-scotland/teenage-booster-immunisation-statistics-scotland-school-year-202223/#:~:text=In%202022%2F23%20an%20additional,77.3%25%2C%20MenACWY%2077.4%25).
+teenage_booster_raw <- import("https://www.opendata.nhs.scot/dataset/807cf7c8-ba2a-49e3-b9d8-055799ecefd2/resource/964b7fe0-28b3-4aa8-a16c-f1ca29c13f85/download/ca-teenb-immunisation-20231128.csv")
 
-tennage_booster_raw <-
-  read_excel(tf, sheet = "Table 2b (LA- S4)", skip = 4)
-
-teenage_booster <-
-  tennage_booster_raw %>%
-  slice(1:32) %>%
-  select(
-    lad_name = `...1`,
-    tp_ipv_teenage_booster_rate = `Uptake rate (%)...4`,
-    menacwy_teenage_booster_rate = `Uptake rate (%)...6`
-  ) %>%
+teenage_booster <- teenage_booster_raw |>
+  filter(Sex == "Total") |>
+  group_by(CA) |>
   mutate(
-    tp_ipv_teenage_booster_rate = as.double(tp_ipv_teenage_booster_rate),
-    menacwy_teenage_booster_rate = as.double(menacwy_teenage_booster_rate)
-  ) %>%
-  mutate(lad_name = str_remove_all(lad_name, "3")) %>%
-  left_join(lookup, by = "lad_name") %>%
-  relocate(lad_code, .after = lad_name) %>%
-  select(-lad_name)
+    PercentCoverage = as.numeric(PercentCoverage),
+    total_av_percentage_coverage = sum(PercentCoverage, na.rm = TRUE) / 4
+    ) |>
+  ungroup() |>
+  rename(year = 1,
+         ltla19_code = 2) |>
+  select(`ltla19_code`, `total_av_percentage_coverage`, `year`) |>
+  slice(1:32)
 
 # Childhood immunisations
-# Source: https://beta.isdscotland.org/find-publications-and-data/population-health/child-health/childhood-immunisation-statistics-scotland/
-GET(
-  "https://beta.isdscotland.org/media/8124/child_imms_latestrates_calendar20_la.xlsx",
-  write_disk(tf <- tempfile(fileext = ".xlsx"))
-)
-
-twelve_months_raw <-
-  read_excel(tf, sheet = "Table 1 - 12m (LA)", skip = 5)
-
-twelve_months <-
-  twelve_months_raw %>%
-  slice(2:33) %>%
+# Population
+population_2022 <- population22_ltla19_scotland |>
+  filter(sex == "Persons") |>
   select(
-    lad_name = `...1`,
-    rate_6_in_1 = `%...4`,
-    rate_pcv = `%...6`,
-    rate_rotavirus = `%...8`,
-    rate_menb = `%...10`
-  ) %>%
-  mutate(lad_name = str_remove_all(lad_name, "4")) %>%
-  mutate(lad_name = str_replace_all(lad_name, "&", "and")) %>%
-  mutate(lad_name = if_else(lad_name == "Edinburgh City", "City of Edinburgh", lad_name)) %>%
-  left_join(lookup, by = "lad_name") %>%
-  relocate(lad_code, .after = lad_name) %>%
-  mutate(across(starts_with("rate_"), as.double)) %>%
-  select(-lad_name)
+    ltla19_name,
+    ltla19_code,
+    population_2022 = all_ages
+  )
 
-twenty_four_months_raw <-
-  read_excel(tf, sheet = "Table 2 - 24m (LA)", skip = 5)
+# Source: https://publichealthscotland.scot/publications/childhood-immunisation-statistics-scotland/childhood-immunisation-statistics-scotland-quarter-ending-30-june-2024/
+url <- "https://publichealthscotland.scot/media/29110/child_imms_latestrates_quarter_224_la.xlsx"
 
-twenty_four_months <-
-  twenty_four_months_raw %>%
-  slice(2:33) %>%
-  select(
-    lad_name = `...1`,
-    rate_6_in_1 = `%...4`,
-    rate_mmr1 = `%...6`,
-    rate_hib_menc = `%...8`,
-    rate_pcvb = `%...10`,
-    rate_menb_booster = `%...12`
-  ) %>%
-  mutate(lad_name = str_remove_all(lad_name, "3")) %>%
-  mutate(lad_name = str_replace_all(lad_name, "&", "and")) %>%
-  mutate(lad_name = if_else(lad_name == "Edinburgh City", "City of Edinburgh", lad_name)) %>%
-  left_join(lookup, by = "lad_name") %>%
-  relocate(lad_code, .after = lad_name) %>%
-  mutate(across(starts_with("rate_"), as.double)) %>%
-  select(-lad_name)
+temp_file <- tempfile(fileext = ".xlsx")
+download.file(url, temp_file, mode = "wb")
 
-three_five_years_raw <-
-  read_excel(tf, sheet = "Table 3 - 5y (LA)", skip = 5)
+# Immunisation uptake rates by 12 months of age
+twelve_months_raw <- read_excel(temp_file, sheet = 2, skip = 4)
 
-three_five_years <-
-  three_five_years_raw %>%
-  slice(2:33) %>%
-  select(
-    lad_name = `...1`,
-    rate_6_in_1 = `%...4`,
-    rate_mmr1 = `%...6`,
-    rate_hib_menc = `%...8`,
-    rate_4_in_1 = `%...10`,
-    rate_mmr2 = `%...12`
-  ) %>%
-  mutate(lad_name = str_remove_all(lad_name, "3")) %>%
-  mutate(lad_name = str_replace_all(lad_name, "&", "and")) %>%
-  mutate(lad_name = if_else(lad_name == "Edinburgh City", "City of Edinburgh", lad_name)) %>%
-  left_join(lookup, by = "lad_name") %>%
-  relocate(lad_code, .after = lad_name) %>%
-  mutate(across(starts_with("rate_"), as.double)) %>%
-  select(-lad_name)
+twelve_months <- twelve_months_raw |>
+  slice(2:33) |>
+  mutate(across(2:10, ~ as.numeric(as.character(.)))) |>
+  rename(ltla19_name = 1,
+         percent_six_in_one = 4,
+         percent_pcv = 6,
+         percent_rotavirus = 8,
+         percent_menb = 10) |>
+  select(`ltla19_name`, `percent_six_in_one`, `percent_pcv`,
+         `percent_rotavirus`, `percent_menb`) |>
+  mutate(year = "2023")
 
-four_six_years_raw <-
-  read_excel(tf, sheet = "Table 4 - 6y (LA)", skip = 5)
+twelve_months <- twelve_months |>
+  mutate(ltla19_name = str_remove(ltla19_name, "\\d+$"))
 
-four_six_years <-
-  four_six_years_raw %>%
-  slice(2:33) %>%
-  select(
-    lad_name = `...1`,
-    rate_mmr1 = `%...4`,
-    rate_4_in_1 = `%...6`,
-    rate_mmr2 = `%...8`
-  ) %>%
-  mutate(lad_name = str_remove_all(lad_name, "3")) %>%
-  mutate(lad_name = str_replace_all(lad_name, "&", "and")) %>%
-  mutate(lad_name = if_else(lad_name == "Edinburgh City", "City of Edinburgh", lad_name)) %>%
-  left_join(lookup, by = "lad_name") %>%
-  relocate(lad_code, .after = lad_name) %>%
-  mutate(across(starts_with("rate_"), as.double)) %>%
-  select(-lad_name)
+# Primary and booster immunisation uptake rates by 24 months of age
+twenty_four_months_raw <- read_excel(temp_file, sheet = 3, skip = 4)
 
-# Combine vaccine indicators and calculate mean
-vaccine_coverage <-
-  hpv %>%
-  left_join(teenage_booster, by = "lad_code") %>%
-  left_join(twelve_months, by = "lad_code") %>%
-  left_join(twenty_four_months, by = "lad_code") %>%
-  left_join(three_five_years, by = "lad_code") %>%
-  left_join(four_six_years, by = "lad_code") %>%
-  pivot_longer(
-    cols = !lad_code,
-    names_to = "vaccine_type",
-    values_to = "rate"
-  ) %>%
-  group_by(lad_code) %>%
-  summarise(vaccine_rate_mean = mean(rate))
+twenty_four_months <- twenty_four_months_raw |>
+  slice(2:33) |>
+  mutate(across(2:12, ~ as.numeric(as.character(.)))) |>
+  rename(ltla19_name = 1,
+         percent_six_in_one = 4,
+         percent_mmr1 = 6,
+         percent_hib_menc = 8,
+         percent_pcvb = 10,
+         percent_menb_booster = 12) |>
+  select(`ltla19_name`, `percent_six_in_one`, `percent_mmr1`,
+         `percent_hib_menc`, `percent_pcvb`, `percent_menb_booster`) |>
+  mutate(year = "2023")
 
-write_rds(vaccine_coverage, "data/vulnerability/health-inequalities/scotland/healthy-lives/vaccination-coverage.rds")
+twenty_four_months <- twenty_four_months |>
+  mutate(ltla19_name = str_remove(ltla19_name, "\\d+$"))
+
+# Primary and booster immunisation uptake rates by 5 years of age
+three_five_years_raw <- read_excel(temp_file, sheet = 4, skip = 4)
+
+three_five_years <- three_five_years_raw |>
+  slice(2:33) |>
+  mutate(across(2:12, ~ as.numeric(as.character(.)))) |>
+  rename(ltla19_name = 1,
+         percent_six_in_one = 4,
+         percent_mmr1 = 6,
+         percent_hib_menc = 8,
+         percent_four_in_one = 10,
+         percent_mmr2 = 12) |>
+  select(`ltla19_name`, `percent_six_in_one`, `percent_mmr1`,
+         `percent_hib_menc`, `percent_four_in_one`, `percent_mmr2`) |>
+  mutate(year = "2023")
+
+three_five_years <- three_five_years |>
+  mutate(ltla19_name = str_remove(ltla19_name, "\\d+$"))
+
+# Primary and booster immunisation uptake rates by 6 years of age
+four_six_years_raw <- read_excel(temp_file, sheet = 5, skip = 4)
+
+four_six_years <- four_six_years_raw |>
+  slice(2:33) |>
+  mutate(across(2:8, ~ as.numeric(as.character(.)))) |>
+  rename(ltla19_name = 1,
+         percent_mmr1 = 4,
+         percent_four_in_one = 6,
+         percent_mmr2 = 8) |>
+  select(`ltla19_name`, `percent_mmr1`, `percent_four_in_one`, `percent_mmr2`) |>
+  mutate(year = "2023")
+
+four_six_years <- four_six_years |>
+  mutate(ltla19_name = str_remove(ltla19_name, "\\d+$"))
+
+# Combine childhood vaccine indicators and calculate mean
+childhood_coverage <-
+  twelve_months |>
+  left_join(twenty_four_months, by = "ltla19_name") |>
+  left_join(three_five_years, by = "ltla19_name") |>
+  left_join(four_six_years, by = "ltla19_name") |>
+  select(-year.x, -year.y, -year.x.x, -year.y.y) |>
+  mutate(total_av_percentage_coverage = rowMeans(across(c(2:18))),
+         year = "2023") |>
+  rename(ltla19_name = 1) |>
+  select(`ltla19_name`, `total_av_percentage_coverage`, `year`)
+
+# Combine LA ID data
+unmatched_LAs <- childhood_coverage |>
+  anti_join(population_2022, by = c("ltla19_name" = "ltla19_name"))
+
+childhood_coverage <- childhood_coverage |>
+  mutate(ltla19_name = str_replace_all(ltla19_name, "&", "and")) |>
+  mutate(ltla19_name = if_else(ltla19_name == "Edinburgh City", "City of Edinburgh", ltla19_name))
+
+childhood_coverage <- childhood_coverage |>
+  left_join(population_2022, by = c("ltla19_name" = "ltla19_name"))
+
+childhood_coverage <- childhood_coverage |>
+  select(`ltla19_code`, `total_av_percentage_coverage`, `year`)
+
+# ---- Join datasets ----
+hl_vaccine_coverage <-
+  hpv |>
+  left_join(teenage_booster, by = "ltla19_code") |>
+  left_join(childhood_coverage, by = "ltla19_code") |>
+  select(-year.x, -year.y) |>
+  group_by(ltla19_code) |>
+  mutate(vaccine_coverage_percentage = (total_av_percentage_coverage.x +
+                                          total_av_percentage_coverage.y +
+                                          total_av_percentage_coverage) / 3) |>
+  select(`ltla19_code`, `vaccine_coverage_percentage`, `year`)
+
