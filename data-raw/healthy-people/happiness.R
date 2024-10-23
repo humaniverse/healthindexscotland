45ee0f2a2e0f989376b9ac0f054901e044b6b990
CatReid <CatrionaReid@redcross.org.uk>
2024-10-23 13:31:28 +0100

    Corrections happiness
    Corrected anxiety data: used 2022-23 data except for Orkney Islands (2020-21). Updated data.R and metadata.

--- data-raw/healthy-people/happiness.R
+++ data-raw/healthy-people/happiness.R
@@ -1,8 +1,7 @@
 # The 'Average (mean)' estimate provides the score out of 0-10. The other
 # estimates are thresholds (percentages) described in the QMI:
 # https://www.ons.gov.uk/peoplepopulationandcommunity/wellbeing/methodologies/personalwellbeingintheukqmi.
-# Orkney Islands missing data in latest collection years. 2020-21 most recent
-# completed data.
+# Orkney Islands missing data in 2022-23; 2020-21 most recent data for Orkney.
 
 # ---- Load packages ----
 library(httr)
@@ -16,19 +15,41 @@ happiness_raw <-
   import("https://download.ons.gov.uk/downloads/datasets/wellbeing-local-authority/editions/time-series/versions/4.csv")
 
 # ---- Clean data ----
-people_happiness <- happiness_raw |>
+# Happiness scores for 2022-23. Data missing for Orkney Islands.
+happiness_2022 <- happiness_raw |>
   filter(
     str_starts(`administrative-geography`, "S"),
     MeasureOfWellbeing == "Happiness",
     Estimate == "Average (mean)",
-    Time == "2020-21"
+    Time == "2022-23"
   ) |>
-  slice(-15) |>
   select(
-    ltla19_code = `administrative-geography`,
+    ltla24_code = `administrative-geography`,
     happiness_score_out_of_10 = `v4_3`,
     year = `Time`
   )
+
+# Happiness scores for 2020-21, with latest Orkney Islands data.
+happiness_orkney <- happiness_raw |>
+  filter(
+    `administrative-geography` == "S12000023",
+    MeasureOfWellbeing == "Happiness",
+    Estimate == "Average (mean)",
+    Time == "2020-21"
+  ) |>
+  select(
+    ltla24_code = `administrative-geography`,
+    happiness_score_out_of_10 = `v4_3`,
+    year = `Time`
+  )
+
+# Combine data
+happiness_2022_filtered <- happiness_2022 |>
+  filter(!(ltla24_code == "S12000023" & year == "2022-23"))
+
+people_happiness <-
+  bind_rows(happiness_2022_filtered,happiness_orkney) |>
+  slice(-15)
 
 # ---- Save output to data/ folder ----
 usethis::use_data(people_happiness, overwrite = TRUE)
