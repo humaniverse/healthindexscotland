e34aa647eb221025c509593d9daa821475a48369
CatReid <CatrionaReid@redcross.org.uk>
2024-10-23 08:14:38 +0100

    Changed LA code: road-safety
    Updated LA code: road-safety

--- data-raw/healthy-places/road-safety.R
+++ data-raw/healthy-places/road-safety.R
@@ -44,7 +44,9 @@ places_road_safety <-
   road_safety |>
   left_join(areas, by = "ltla21_code") |>
   mutate(road_accident_count_by_area = accident_count / area_km_squared) |>
-  select(ltla21_code, road_accident_count_by_area) |>
+  select(
+    ltla24_code = ltla21_code,
+    road_accident_count_by_area) |>
   mutate(year = 2022)
 
 # ---- Save output to data/ folder ----
