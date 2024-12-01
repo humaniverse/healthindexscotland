8fa4f66146cb05a9db9124f4e1ce1d611ba4ebcd
KarolisKoncevicius <karolis.koncevicius@gmail.com>
2024-12-01 21:53:50 +0200

    adjusted PCA-varimax index to be build for each domain (lives, people, places) separately and then combined

--- data-raw/build-index-pca.R
+++ data-raw/build-index-pca.R
@@ -1,37 +1,33 @@
 #--- method --------------------------------------------------------------------
-
-# Adapted from:
-# https://www.oecd-ilibrary.org/docserver/9789264043466-en.pdf?expires=1732447217&id=id&accname=guest&checksum=C9FB3C0E94A6DE6D49C811382692119B
 #
-# Steps:
-#   1. 
+# # Adapted from:
+# # https://www.oecd-ilibrary.org/docserver/9789264043466-en.pdf?expires=1732447217&id=id&accname=guest&checksum=C9FB3C0E94A6DE6D49C811382692119B
 #
 # library(psych)
 #
-# toy dataset
+# # toy dataset
 # dat <- read.csv("example_data.csv")
 #
-# recreating correlations (page 64)
+# # recreating correlations
 # cor(dat[-1])
 #
-# recreating pca (page 65)
+# # recreating pca
 # pca <- prcomp(dat[-1], center = TRUE, scale. = TRUE)
 #
-# percent variance explained
+# # percent variance explained
 # pca$sdev^2 / sum(pca$sdev^2)
 #
-# factors (page 90)
+# # factors
 # fa <- principal(cor(dat[-1]), 4)
 # l <- apply(unclass(fa$loadings)^2, 2, function(x) x/sum(x))
 # l[l < 0.2] <- 0
 # l <- apply(l, 2, function(x) x/sum(x))
 # w <- fa$Vaccounted[1,] / sum(fa$Vaccounted[1,])
-# l * w[col(l)]
 #
 # colSums(t(l) %*% t(apply(data.matrix(dat[-1]), 2, scale)) * w)
 #
-# NOTE: cannot replicate the final rankings of TAI example
-# reason - not sure how they were normalized
+# # NOTE: cannot replicate the final rankings of TAI example
+# # reason - not sure how they were normalized
 
 
 #--- libraries -----------------------------------------------------------------
@@ -90,25 +86,63 @@ indicators[is.na(indicators)] <- 0
 
 #--- combine -------------------------------------------------------------------
 
-# pca
-pca <- prcomp(indicators[-1], center = TRUE, scale. = TRUE)
-# number of components selected as explained % of cummulative variance > 60 %
-npc <- which(cumsum(round(pca$sdev^2 / sum(pca$sdev^2) * 100)) > 60)[1]
+build_pc_indicators <- function(x) {
+  # pca
+  # center and scale to make sure all indicators are weighted equally
+  pca <- prcomp(x, center = TRUE, scale. = TRUE)
+  # determine the number of components selected (cummulative variance > 60 %)
+  npc <- which(cumsum(round(pca$sdev^2 / sum(pca$sdev^2) * 100)) > 60)[1]
 
-# factors
-fa <- principal(cor(indicators[-1]), npc)
-fw <- fa$Vaccounted[1,] / sum(fa$Vaccounted[1,])
-l <- apply(unclass(fa$loadings)^2, 2, function(x) x/sum(x))
-l <- t(apply(l, 1, function(x) {x[x < max(x)] <- 0; x}))
-l <- apply(l, 2, function(x) x/sum(x))
-l <- l * fw[col(l)]
-w <- apply(l, 1, max)
+  # factors
+  # NOTE: based on replication from OECD toy dataset example
+  # NOTE: using psych::principal() since it aligned with OECD results
+  fa <- principal(cor(x), npc)
+  # variance explained by each factor
+  fw <- fa$Vaccounted[1,] / sum(fa$Vaccounted[1,])
+
+  # square loadings and make sure they sum to 1
+  l <- apply(unclass(fa$loadings)^2, 2, function(x) x/sum(x))
+  # for each indicator only leave the highest weight
+  l <- t(apply(l, 1, function(x) {x[x < max(x)] <- 0; x}))
+  # scale again so each factor sums to 1
+  l <- apply(l, 2, function(x) x/sum(x))
+  # multiply by variance explain
+  # NOTE: not sure why, but it's in the OECD example
+  l <- l * fw[col(l)]
+
+  # indicator weights
+  w <- apply(l, 1, max)
+
+  # final score
+  colSums(t(x) * w)
+}
+
+quantise <- function(x) {
+  findInterval(x, quantile(x, seq(0,1,0.1)), rightmost.closed = TRUE)
+}
+
 
 scores <- data.frame(ltla24_code = indicators$ltla24_code)
 
-scores$score    <- colSums(t(indicators[-1]) * w)
-scores$rank     <- rank(scores$score)
-scores$quantile <- as.numeric(cut(scores$rank, quantile(scores$rank, seq(0,1,0.1)), include.lowest = TRUE))
+# healthy lives
+scores$healthy_lives_score    <- build_pc_indicators(select(indicators, starts_with("lives")))
+scores$healthy_lives_rank     <- rank(scores$healthy_lives_score)
+scores$healthy_lives_quantile <- quantise(scores$healthy_lives_rank)
+
+# healthy places
+scores$healthy_places_score    <- build_pc_indicators(select(indicators, starts_with("places")))
+scores$healthy_places_rank     <- rank(scores$healthy_places_score)
+scores$healthy_places_quantile <- quantise(scores$healthy_places_rank)
+
+# healthy people
+scores$healthy_people_score    <- build_pc_indicators(select(indicators, starts_with("people")))
+scores$healthy_people_rank     <- rank(scores$healthy_people_score)
+scores$healthy_people_quantile <- quantise(scores$healthy_people_rank)
+
+# combined
+scores$health_inequalities_score    <- rowSums((select(scores, ends_with("_rank")) - 1) / (nrow(scores) - 1))
+scores$health_inequalities_rank     <- rank(scores$health_inequalities_score)
+scores$health_inequalities_quantile <- quantise(scores$health_inequalities_rank)
 
 
 #--- save ----------------------------------------------------------------------
