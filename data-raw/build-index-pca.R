835fdd7c112f61c76c041459841f7b692ae67ba6
KarolisKoncevicius <karolis.koncevicius@gmail.com>
2024-12-08 09:05:57 +0200

    adjusted PCA to be done for sub-domains and not domains

--- data-raw/build-index-pca.R
+++ data-raw/build-index-pca.R
@@ -87,6 +87,10 @@ indicators[is.na(indicators)] <- 0
 #--- combine -------------------------------------------------------------------
 
 build_pc_indicators <- function(x) {
+  # NOTE: if only one indicator - return it
+  if(ncol(x) == 1) {
+    return(x[[1]])
+  }
   # pca
   # center and scale to make sure all indicators are weighted equally
   pca <- prcomp(x, center = TRUE, scale. = TRUE)
@@ -103,7 +107,7 @@ build_pc_indicators <- function(x) {
   # square loadings and make sure they sum to 1
   l <- apply(unclass(fa$loadings)^2, 2, function(x) x/sum(x))
   # for each indicator only leave the highest weight
-  l <- t(apply(l, 1, function(x) {x[x < max(x)] <- 0; x}))
+  l[l < apply(l, 1, max)] <- 0
   # scale again so each factor sums to 1
   l <- apply(l, 2, function(x) x/sum(x))
   # multiply by variance explain
@@ -121,26 +125,47 @@ quantise <- function(x) {
   findInterval(x, quantile(x, seq(0,1,0.1)), rightmost.closed = TRUE)
 }
 
+# subdomains
+subdomains <- list()
+subdomains$people_difficulties_in_daily_life <- indicators[c("people_disability")]  # NOTE: missing frailty
+subdomains$people_mental_health              <- indicators[c("people_suicides")]    # NOTE: missing self-harm
+subdomains$people_mortality                  <- indicators[c("people_avoidable_deaths", "lives_infant_mortality", "people_healthy_life_expectancy", "people_all_mortality")]  # NOTE: infant mortality in "lives" not "people"
+subdomains$people_personal_wellbeing         <- indicators[c("people_life_worthwhileness", "people_anxiety", "people_happiness", "people_life_satisfaction")]
+subdomains$people_physical_health_conditions <- indicators[c("people_life_worthwhileness", "lives_cardiovascular_conditions", "people_dementia", "people_musculoskeletal_conditions")]  # NOTE: cardio in lives, diabetes, kidney, and respiratory are missing
+
+subdomains$lives_behavioural_risk_factors   <- indicators[c("lives_alcohol_misuse", "lives_drug_misuse", "lives_healthy_eating", "lives_physical_activity", "lives_sedentary_behaviour", "lives_smoking")]  # NOTE: sexual health is missing
+subdomains$lives_children_and_younr_people  <- indicators[c("lives_pupil_absence", "lives_national_five_attainment", "lives_teenage_pregnancy", "lives_young_people_training")]
+subdomains$lives_psychological_risk_factors <- indicators[c("lives_high_blood_pressure", "lives_low_birth_weight", "lives_overweight_obesity_adults", "lives_overweight_obesity_children")]
+subdomains$lives_protective_measures        <- indicators[c("lives_cancer_screening", "lives_child_vaccine_coverage")]
+
+subdomains$places_access_to_green_space           <- indicators[c("places_private_outdoor_space")]
+subdomains$places_access_to_services              <- indicators[c("places_gp_travel_time", "places_pharmacy_travel_time", "places_sports_centre_travel_time", "places_internet_access")]  # NOTE: missing acceptable gp appointments
+subdomains$places_crime                           <- indicators[c("places_low_level_crime", "places_personal_crime")]
+subdomains$places_economic_and_working_conditions <- indicators[c("lives_child_poverty", "lives_job_training", "lives_unemployment", "lives_workplace_safety")]  # NOTE: all in lives
+subdomains$places_living_conditions               <- indicators[c("places_air_pollution", "places_household_overcrowding", "places_road_safety", "places_rough_sleeping")]  # NOTE: noise complaints is missing
+
+subdomains <- data.frame(ltla24_code = indicators$ltla24_code, sapply(subdomains, build_pc_indicators))
+
 
 scores <- data.frame(ltla24_code = indicators$ltla24_code)
 
 # healthy lives
-scores$healthy_lives_score    <- build_pc_indicators(select(indicators, starts_with("lives")))
+scores$healthy_lives_score    <- rowSums(select(subdomains, starts_with("lives")))
 scores$healthy_lives_rank     <- rank(scores$healthy_lives_score)
 scores$healthy_lives_quantile <- quantise(scores$healthy_lives_rank)
 
 # healthy places
-scores$healthy_places_score    <- build_pc_indicators(select(indicators, starts_with("places")))
+scores$healthy_places_score    <- rowSums(select(subdomains, starts_with("places")))
 scores$healthy_places_rank     <- rank(scores$healthy_places_score)
 scores$healthy_places_quantile <- quantise(scores$healthy_places_rank)
 
 # healthy people
-scores$healthy_people_score    <- build_pc_indicators(select(indicators, starts_with("people")))
+scores$healthy_people_score    <- rowSums(select(subdomains, starts_with("people")))
 scores$healthy_people_rank     <- rank(scores$healthy_people_score)
 scores$healthy_people_quantile <- quantise(scores$healthy_people_rank)
 
 # combined
-scores$health_inequalities_score    <- rowSums((select(scores, ends_with("_rank")) - 1) / (nrow(scores) - 1))
+scores$health_inequalities_score    <- rowSums(select(scores, ends_with("_score")))
 scores$health_inequalities_rank     <- rank(scores$health_inequalities_score)
 scores$health_inequalities_quantile <- quantise(scores$health_inequalities_rank)
 
